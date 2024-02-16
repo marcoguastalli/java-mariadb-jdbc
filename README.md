7fa52684b82e9bff896a570467a4f5ef365a1006
marcoguastalli <marco.guastalli@gmail.com>
2024-02-16 10:20:10 +0100

    added ConnectToMySql

--- README.md
+++ README.md
@@ -1,22 +1,8 @@
 # java-mariadb-jdbc
 Java connect to MariaDB using JDBC Drivers
 
-`mvn clean package`
-
-
-`colima start --cpu 4 --memory 8`
-`cd ~/dev/repository/git/docker27/mariadb/src/v1`
-`docker-compose up -d`
-`docker-compose down -v`
-`colima stop`
-
-http://marco27.net/
-http://marco27.net/index.php
-http://marco27.net:8000
-mariadb
-root:root123
-http://marco27.net:8000/index.php?route=/database/structure&db=db
-
+`mysql.server start`
+`mysql.server stop`
 
 ```
 <bean id="dataSource" class="org.apache.commons.dbcp2.BasicDataSource">
---
+++ src/main/java/robots/ConnectToMySql.java
@@ -0,0 +1,36 @@
+package robots;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+
+import exception.RobotsRuntimeException;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+public class ConnectToMySql {
+
+    private static final Logger logger = LogManager.getLogger(ConnectToMySql.class);
+    public static void main(String[] args) {
+        String url = "jdbc:mysql://localhost:3306/novo?useSSL=false";
+        String user = "novo";
+        String password = "novo123";
+        try (Connection conn = DriverManager.getConnection(url, user, password)){
+            Statement stmt = conn.createStatement();
+            stmt.execute("SELECT * FROM books");
+
+            ResultSet rs = stmt.getResultSet();
+            while (rs.next()) {
+                long id = rs.getLong("BookID");
+                String title = rs.getString("Title");
+                logger.info(id + " " + title);
+            }
+            stmt.close();
+        } catch (SQLException e) {
+            logger.error("SQL Error", e);
+            throw new RobotsRuntimeException("SQL Error", e);
+        }
+    }
+}
